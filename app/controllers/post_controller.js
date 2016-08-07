113ec6b6f4896dd4f54177281c7b0077e03c65ec
Chris Davis <christopher.o.davis.18@dartmouth.edu>
2016-08-07 15:37:43 -0400

    create posts working

---
+++ app/controllers/post_controller.js
@@ -0,0 +1,26 @@
+import Post from '../models/post_model';
+
+
+export const createPost = (req, res) => {
+  const post = new Post();
+  post.title = req.body.title;
+  post.save()
+  .then(result => {
+    res.json({ message: 'Post created!' });
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+export const getPosts = (req, res) => {
+  res.send('posts should be returned');
+};
+export const getPost = (req, res) => {
+  res.send('single post looked up');
+};
+export const deletePost = (req, res) => {
+  res.send('delete a post here');
+};
+export const updatePost = (req, res) => {
+  res.send('update a post here');
+};
