4070dfbd322b8db59f839bd22ec4f0dc8b5bf1dc
Chris Davis <christopher.o.davis.18@dartmouth.edu>
2016-08-09 15:20:54 -0400

    hopefully final fix

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -37,7 +37,7 @@ export const getPosts = (req, res) => {
 
 
 export const getPost = (req, res) => {
-  Post.findbyID({ _id: req.params.id })
+  Post.findByID({ _id: req.params.id })
     .then(post => {
       res.json(cleanPost(post));
     })
