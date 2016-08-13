55a3658bb57f4b9268b171926449f415b3117821
Chris Davis <christopher.o.davis.18@dartmouth.edu>
2016-08-13 16:08:32 -0400

    backend auth good

---
+++ app/controllers/user_controller.js
@@ -0,0 +1,45 @@
+import jwt from 'jwt-simple';
+import User from '../models/user_model';
+import config from '../config';
+
+// encodes a new token for a user object
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+export const signin = (req, res, next) => {
+  res.send({ token: tokenForUser(req.user) });
+};
+
+export const signup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send('You must provide email and password');
+  }
+
+  // 🚀 TODO:
+  // here you should do a mongo query to find if a user already exists with this email.
+  // if user exists then return an error. If not, use the User model to create a new user.
+  // Save the new User object
+  // this is similar to how you created a Post
+  // and then return a token same as you did in in signin
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const newUser = new User();
+        newUser.email = email;
+        newUser.password = password;
+        newUser.save()
+          .then(() => {
+            res.send({ token: tokenForUser(newUser) });
+          });
+      } else {
+        console.log(`USER ALREADY EXISTS: ${user}`);
+        return res.status(422).send('User already exists');
+      }
+    });
+};
--- app/models/post_model.js
+++ app/models/post_model.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 // create a schema for posts with a field
 const PostSchema = new Schema({
   title: String,
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
   tags: String,
   content: String,
 });
